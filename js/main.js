37894f95af9680bb94346528c0f5f0ff44de5ba7
Maja1rus <nalimoff.deniss@gmail.com>
2021-06-05 18:31:01 +0300

    download

---
+++ js/main.js
@@ -0,0 +1,181 @@
+const DATA = [{
+        questions: 'Вопрос 1',
+        answers: [{
+                id: '1',
+                value: 'Ответ 1',
+                correct: true,
+            },
+            {
+                id: '2',
+                value: 'Ответ 2',
+                correct: false,
+            },
+            {
+                id: '3',
+                value: 'Ответ 3',
+                correct: false,
+            },
+
+        ]
+    },
+    {
+        questions: 'Вопрос 2',
+        answers: [{
+                id: '4',
+                value: 'Ответ 1',
+                correct: false,
+            },
+            {
+                id: '5',
+                value: 'Ответ 2',
+                correct: true,
+            },
+        ]
+    },
+    {
+        questions: 'Вопрос 3',
+        answers: [{
+                id: '6',
+                value: 'Ответ 1',
+                correct: true,
+            },
+            {
+                id: '7',
+                value: 'Ответ 2',
+                correct: false,
+            },
+        ]
+    },
+    {
+        questions: 'Вопрос 4',
+        answers: [{
+                id: '8',
+                value: 'Ответ 1',
+                correct: false,
+            },
+            {
+                id: '9',
+                value: 'Ответ 2',
+                correct: true,
+            },
+            {
+                id: '10',
+                value: 'Ответ 3',
+                correct: false,
+            },
+        ]
+    },
+];
+
+let localResults = {};
+
+const quiz = document.getElementById('quiz');
+const questions = document.getElementById('questions');
+const indicator = document.getElementById('indicator');
+const results = document.getElementById('results');
+const btnNext = document.getElementById('btn-next');
+const btnRestart = document.getElementById('btn-restart');
+
+const renderQuestins = (index) => {
+    renderIndicator(index + 1);
+
+    questions.dataset.currentStep = index;
+
+    const renderAnswers = () => DATA[index].answers
+        .map((answer) => `
+        <li>
+            <label for="#">
+                <input class="answer-input" type="radio" name=${index} value=${answer.id}>
+                ${answer.value}
+            </label>
+        </li>  
+    `)
+        .join('');
+
+    questions.innerHTML = `
+            <div class="quiz-questions-item">
+                <div class="quiz-questions-item__question">${DATA[index].questions}</div>
+                <ul class="quiz-questions-item__answers">${renderAnswers()}</ul>
+            </div>
+    `;
+};
+
+const renderResults = () => {
+    let content = '';
+
+    const getClassName = (answer, questionIndex) => {
+        let className = '';
+
+        if (!answer.correct && answer.id === localResults[questionIndex]) {
+            className = 'answer--invalid';
+        } else if (answer.correct) {
+            className = 'answer--valid';
+        }
+
+        return className;
+    }
+
+    const getAnswers = (questionIndex) => DATA[questionIndex].answers
+        .map((answer) => `<li class=${getClassName(answer, questionIndex)}>${answer.value}</li>`)
+        .join('');
+
+    DATA.forEach((question, index) => {
+        content += `
+            <div class="quiz-results-item">
+                <div class="quiz-results-item__question">${question.questions}</div>
+                <ul class="quiz-results-item__answers">${getAnswers(index)}</ul>
+            </div>
+        `;
+    });
+
+    results.innerHTML = content;
+};
+
+const renderIndicator = (currentStep) => {
+    indicator.innerHTML = `${currentStep}/${DATA.length}`;
+};
+
+quiz.addEventListener('change', (event) => {
+    //Логика ответа
+    if (event.target.classList.contains('answer-input')) {
+        localResults[event.target.name] = event.target.value;
+        btnNext.disabled = false;
+    }
+});
+
+quiz.addEventListener('click', (event) => {
+    //Далее или С начала
+    if (event.target.classList.contains('btn-next')) {
+        const nextQuestionIndex = Number(questions.dataset.currentStep) + 1;
+
+        if (DATA.length === nextQuestionIndex) {
+            //переход к результатам
+            questions.classList.add('questions--hidden');
+            indicator.classList.add('indicator--hidden');
+            results.classList.add('results--visible');
+            btnNext.classList.add('btn-next--hidden');
+            btnRestart.classList.add('btn-restart--visible');
+            renderResults();
+        } else {
+            //переход к следующему вопросу
+            renderQuestins(nextQuestionIndex);
+        }
+
+        btnNext.disabled = true;
+    }
+
+    if (event.target.classList.contains('btn-restart')) {
+        localResults = {};
+        results.innerHTML = '';
+
+        questions.classList.remove('questions--hidden');
+        indicator.classList.remove('indicator--hidden');
+        results.classList.remove('results--visible');
+        btnNext.classList.remove('btn-next--hidden');
+        btnRestart.classList.remove('btn-restart--visible');
+
+        renderQuestins(0);
+    }
+});
+
+renderQuestins(0);
